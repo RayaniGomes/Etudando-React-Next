93fa2c22c2e3b2de3007bdd705950f8e58435ff8
Rayani Gomes <68917577+RayaniGomes@users.noreply.github.com>
2024-09-06 16:57:40 -0300

    continuação do estudo

--- JAVASCRIPT POO/ContaCorrente.js
+++ JAVASCRIPT POO/ContaCorrente.js
@@ -1,20 +1,46 @@
 export class ContaCorrente{
     agencia;
+    cliente; 
     saldo = 0;
+
+    set Cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this.cliente = novoValor;
+        }
+    }
+
+    get Cliente(){
+        return this.cliente;
+    }
+
+    get Saldo(){
+        return this.saldo;
+    }
+    
     sacar(valor){
         if(this.saldo >= valor){
             this.saldo -= valor;
-            console.log(`Saque realizado. Saldo atual: R$${this.saldo},00`);
+            return console.log(`Saque: R$${valor} \nSaldo atual: R$${this.saldo},00`);
         }else{
-            console.log(`Você só tem: R$${this.saldo},00, saldo insuficiente para sacar R$${valor},00 `);
+            console.log(`Você tem: R$${this.saldo},00 \nSaldo insuficiente para sacar R$${valor},00 `);
         }
     }
 
     depositar(valor){
         if(valor > 0){
             this.saldo += valor;
+            return console.log(`Deposito: R$${valor} \nSaldo atual: R$${this.saldo},00`);
+        }
+    }
 
-            console.log(`Depósito realizado. Saldo atual: R$${this.saldo},00`);
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+
+        if (valorSacado != 0) {
+            console.log(`Transferência realizada para ${conta.cliente.nome}: R$${valor}`);
+        }else{
+            console.log('Nenhuma transferência realizada!');
         }
     }
 }
--- JAVASCRIPT POO/index.js
+++ JAVASCRIPT POO/index.js
@@ -5,13 +5,23 @@ const cliente1 = new Cliente();
 cliente1.nome = "João";
 cliente1.cpf = 11111111111;
 
+// const cliente2 = new Cliente();
+// cliente2.nome = "Maria";
+// cliente2.cpf = 22222222222;
+
 const contaCorrente1 = new ContaCorrente();
-contaCorrente1.saldo = 100;
+contaCorrente1.cliente = cliente1;
 contaCorrente1.agencia = 1111;
+contaCorrente1.depositar(500);
 
-contaCorrente1.depositar(100);
-contaCorrente1.sacar(50);
+const contaCorrente2 = new ContaCorrente();
+contaCorrente2.cliente = new Cliente();
+contaCorrente2.cliente.nome = "Luiza";
+contaCorrente2.cliente.cpf = 33333333333;
+contaCorrente2.agencia = 2222;
+ 
+contaCorrente1.transferir(200, contaCorrente2);
 
-console.log(contaCorrente1);
 
-//parei no modulo 3
+console.log(contaCorrente2.saldo);
+
